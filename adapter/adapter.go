f3b487f4e384357eee5af9b3e0313613e675c1e3
Joe Chen <jc@unknwon.io>
2021-12-11 15:56:27 +0800

    adapter: add methods to `Executor` interface (#11)

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -4,6 +4,13 @@
 
 package adapter
 
+import (
+	"context"
+	"database/sql"
+
+	"unknwon.dev/norm/internal/exql"
+)
+
 // Name is the name of the database adapter.
 type Name string
 
@@ -29,6 +36,26 @@ type Adapter interface {
 // Executor compiles query statements into actual SQL queries and executes them
 // using the underlying database driver.
 type Executor interface {
+	// Exec compiles the statement to a query and executes it without returning any
+	// rows. The args are for any placeholder parameters in the query.
+	Exec(ctx context.Context, stmt *exql.Statement, args ...interface{}) (sql.Result, error)
+	// Prepare compiles and prepares the statement for later queries or executions.
+	//
+	// Multiple queries or executions may be run concurrently from the returned
+	// statement. The caller must call the statement's Close method when the
+	// statement is no longer needed.
+	Prepare(ctx context.Context, stmt *exql.Statement) (*sql.Stmt, error)
+	// Query compiles the statement to a query and executes it for returning rows.
+	// The args are for any placeholder parameters in the query.
+	Query(ctx context.Context, stmt *exql.Statement, args ...interface{}) (*sql.Rows, error)
+	// QueryRow compiles the statement to a query and executes it for returning at
+	// most one row.
+	//
+	// This method always returns a non-nil value, and errors are deferred until
+	// `(*sql.Row).Scan` method is called. If the query selects no rows, the
+	// `(*sql.Row).Scan` will return `sql.ErrNoRows`. Otherwise, the
+	// `(*sql.Row).Scan` scans the first selected row and discards the rest.
+	QueryRow(ctx context.Context, stmt *exql.Statement, args ...interface{}) (*sql.Row, error)
 }
 
 // Typer transparently wraps types for scanning and storing values from and to
