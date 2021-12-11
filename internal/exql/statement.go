c3324b5b63bed22a5ad075c3ed2adc03bcc30c3c
Joe Chen <jc@unknwon.io>
2021-12-11 15:54:49 +0800

    adapter: add methods to `Executor` interface

---
+++ internal/exql/statement.go
@@ -0,0 +1,9 @@
+// Copyright 2012 The upper.io/db authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package exql
+
+// Statement is an AST for constructing SQL statements.
+type Statement struct {
+}
