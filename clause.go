6a88a7eea45711f2200103c2fa0579c05d2f9a87
Joe Chen <jc@unknwon.io>
2021-12-12 15:24:12 +0800

    selector: define methods of the `Selector` interface (#12)

--- clause.go
+++ clause.go
@@ -5,8 +5,183 @@
 
 package norm
 
+import (
+	"context"
+)
+
 // Selector represents a SQL query builder for the SELECT statement.
 type Selector interface {
+	// Columns defines which columns to retrieve. Subsequent calls to Columns()
+	// append more columns to the retrieval list (i.e. do not replace previously set
+	// columns).
+	//
+	// The From() should be called along with Columns() to query data from a
+	// specific table:
+	//
+	//   q.Columns("name", "last_name").From("users")
+	//
+	// Aliases may be used for the columns using the "AS" keyword:
+	//
+	//   q.Columns("name AS n")
+	//
+	// or using the shortcut:
+	//
+	//   q.Columns("name n")
+	//
+	// Use the `expr.Raw` to prevent column expressions to be escaped:
+	//
+	//   q.Columns(expr.Raw("MAX(id)"))
+	//
+	// The above statement is equivalent to:
+	//
+	//   q.Columns(expr.Func("MAX", "id"))
+	Columns(columns ...interface{}) Selector
+	// From constructs a FROM clause for where the data to be retrieved from.
+	//
+	// It is typically used along with Columns():
+	//
+	//   q.Columns(...).From("users")
+	//
+	// It is also possible to use an alias for the table:
+	//
+	//   q.Columns(...).From("users AS u").Where("u.name = ?", ...)
+	//
+	// or using the shortcut:
+	//
+	//    q.Columns(...).From("users u").Where("u.name = ?", ...)
+	From(tables ...interface{}) Selector
+	// Distinct constructs a DISTINCT clause with given columns.
+	//
+	// It is used to ask the database to return only values that are different:
+	//
+	//    q.From(...).Distinct("name")
+	Distinct(columns ...interface{}) Selector
+	// As constructs an alias for the table.
+	//
+	// It can only be called after From() which defines the table:
+	//
+	//   q.From("users").As("u")
+	As(alias string) Selector
+
+	// Where constructs a WHERE clause to specify the conditions that columns must
+	// match in order to be retrieved.
+	//
+	// It accepts raw strings and `fmt.Stringer` to define conditions and uses
+	// `interface{}` to specify bind parameters.
+	//
+	// CAUTION: It is possible to cause SQL injection when embedding values of bind
+	// parameters to the SQL query string. The placeholder question mark ("?")
+	// should be used instead:
+	//
+	//   q.Where("name = ?", "max")
+	//   q.Where("name = ? AND last_name = ?", "Mary", "Doe")
+	//   q.Where("last_name IS NULL")
+	//   q.Where("online = ? AND last_logged <= ?", true, time.Now())
+	//
+	// Subsequent calls to Where() replace previously set conditions, use And()
+	// instead for condition conjunctions.
+	Where(conds ...interface{}) Selector
+	// And appends more conditions to the WHERE clause.
+	//
+	// It can be called regardless of Where():
+	//
+	//   q.Where("name = ?", "max").And("last_name = ?", "Doe")
+	//   q.And("last_name = ?", "Doe")
+	And(conds ...interface{}) Selector
+	// GroupBy constructs a GROUP BY clause.
+	//
+	// It defines which columns should be used to aggregate and group results:
+	//
+	//   q.GroupBy("country_id")
+	//   q.GroupBy("country_id", "city_id")
+
+	GroupBy(columns ...interface{}) Selector
+	// OrderBy constructs a ORDER BY clause.
+	//
+	// It is used to define which columns are going to be used to sort results, and
+	// results are in ascending order by default:
+	//
+	//   => ORDER BY "last_name" ASC
+	//   q.OrderBy("last_name")
+	//   q.OrderBy("last_name ASC")
+	//
+	//   => ORDER BY "last_name" DESC, "name" ASC
+	//   q.OrderBy("last_name DESC", "name ASC")
+	//
+	// Prefix the column name with the minus sign ("-") to sort results in
+	// descending order:
+	//
+	//   => ORDER BY "last_name" DESC
+	//   q.OrderBy("-last_name")
+	OrderBy(columns ...interface{}) Selector
+	// Join constructs a JOIN clause.
+	//
+	// JOIN statements are used to define external tables to be included as part of
+	// the result:
+	//
+	//   q.Join("authors")
+	//
+	// Use the On() to specify the JOIN conditions:
+	//
+	//   q.Join("authors").On("authors.id = books.author_id")
+	//
+	//
+	// Use the Using() to specify the JOIN columns:
+	//
+	//   q.Join("employees").Using("department_id")
+	//
+	// The NATURAL JOIN is used when no conditions specified for the join.
+	Join(table ...interface{}) Selector
+	// FullJoin is similar to Join() but is for FULL JOIN.
+	FullJoin(table ...interface{}) Selector
+	// CrossJoin is similar to Join() but is for CROSS JOIN.
+	CrossJoin(table ...interface{}) Selector
+	// RightJoin is similar to Join() but is for RIGHT JOIN.
+	RightJoin(table ...interface{}) Selector
+	// LeftJoin is similar to Join() but is for LEFT JOIN.
+	LeftJoin(table ...interface{}) Selector
+	// On constructs a ON clause.
+	//
+	// It can only be called after any type of join method and accepts the same
+	// types of arguments as Where():
+	//
+	//   q.Join(...).On("b.author_id = a.id")
+	On(conds ...interface{}) Selector
+	// Using constructs a USING clause.
+	//
+	// It is used to specify columns to join results and can only be called after
+	// any type of join method:
+	//
+	//   q.LeftJoin(...).Using("country_id")
+	Using(columns ...interface{}) Selector
+
+	// Limit constructs a LIMIT clause.
+	//
+	// It is used to define the maximum number of rows to be returned from the
+	// query:
+	//
+	//   q.Limit(42)
+	//
+	// A negative limit cancels any previously set limit.
+	Limit(int) Selector
+	// Offset constructs a OFFSET clause.
+	//
+	// It is used to define how many results are going to be skipped before starting to
+	// return results.
+	//
+	//   q.Offset(56)
+	//
+	// A negative offset cancels any previously set offset.
+	Offset(int) Selector
+
+	// Iterate creates an Iterator to iterate over query results.
+	Iterate(ctx context.Context) Iterator
+	ResultMapper
+
+	// String returns a complied SQL query string.
+	String() string
+	// Arguments returns the arguments that are prepared for this query.
+	Arguments() []interface{}
 }
 
 // Inserter represents a SQL query builder for the INSERT statement.
@@ -19,4 +194,31 @@ type Updater interface {
 
 // Deleter represents a SQL query builder for the DELETE statement.
 type Deleter interface {
+}
+
+// Iterator defines a collection of methods to iterate over query results.
+type Iterator interface {
+	ResultMapper
+}
+
+// ResultMapper defines a collection of methods to map query results to
+// destination objects.
+type ResultMapper interface {
+	// All dumps all the results into the destination, and expects a pointer to the
+	// slice of maps or structs.
+	//
+	// The behaviour of One() is extended to each one of the results.
+	All(ctx context.Context, destSlice interface{}) error
+
+	// One maps the row that is in the current query cursor into the destination,
+	// which can be a pointer to a map or a struct.
+	//
+	// If dest is a pointer to a map, each column creates a new map key and the
+	// results are set as values of the keys. Depending on the type of map key and
+	// value, the results columns and values may need to be transformed.
+	//
+	// If dest is a pointer to a struct, each one field will be tested for a `db`
+	// tag which defines the column name mapping. The results are set as values of
+	// the fields.
+	One(ctx context.Context, dest interface{}) error
 }
