7755cf368cc2e6999bc34c0d30902d07c5aaed04
Joe Chen <jc@unknwon.io>
2021-12-16 01:28:47 +0800

    WIP

--- internal/exql/column_test.go
+++ internal/exql/column_test.go
@@ -26,7 +26,7 @@ func TestColumnString(t *testing.T) {
 
 	column := Column{Name: "role.name"}
 
-	s, err := column.Compile(defaultTemplate)
+	s, err := column.Compile(defaultTemplate())
 	if err != nil {
 		t.Fatal()
 	}
@@ -40,7 +40,7 @@ func TestColumnString(t *testing.T) {
 func TestColumnAs(t *testing.T) {
 	column := Column{Name: "role.name as foo"}
 
-	s, err := column.Compile(defaultTemplate)
+	s, err := column.Compile(defaultTemplate())
 	if err != nil {
 		t.Fatal()
 	}
@@ -54,7 +54,7 @@ func TestColumnAs(t *testing.T) {
 func TestColumnImplicitAs(t *testing.T) {
 	column := Column{Name: "role.name foo"}
 
-	s, err := column.Compile(defaultTemplate)
+	s, err := column.Compile(defaultTemplate())
 	if err != nil {
 		t.Fatal()
 	}
@@ -68,7 +68,7 @@ func TestColumnImplicitAs(t *testing.T) {
 func TestColumnRaw(t *testing.T) {
 	column := Column{Name: Raw{Value: "role.name As foo"}}
 
-	s, err := column.Compile(defaultTemplate)
+	s, err := column.Compile(defaultTemplate())
 	if err != nil {
 		t.Fatal()
 	}
@@ -76,47 +76,5 @@ func TestColumnRaw(t *testing.T) {
 	e := `role.name As foo`
 	if s != e {
 		t.Fatalf("Got: %s, Expecting: %s", s, e)
-	}
-}
-
-func BenchmarkColumnHash(b *testing.B) {
-	c := Column{Name: "name"}
-	for i := 0; i < b.N; i++ {
-		c.Hash()
-	}
-}
-
-func BenchmarkColumnCompile(b *testing.B) {
-	c := Column{Name: "name"}
-	for i := 0; i < b.N; i++ {
-		_, _ = c.Compile(defaultTemplate)
-	}
-}
-
-func BenchmarkColumnCompileNoCache(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		c := Column{Name: "name"}
-		_, _ = c.Compile(defaultTemplate)
-	}
-}
-
-func BenchmarkColumnWithDotCompile(b *testing.B) {
-	c := Column{Name: "role.name"}
-	for i := 0; i < b.N; i++ {
-		_, _ = c.Compile(defaultTemplate)
-	}
-}
-
-func BenchmarkColumnWithImplicitAsKeywordCompile(b *testing.B) {
-	c := Column{Name: "role.name foo"}
-	for i := 0; i < b.N; i++ {
-		_, _ = c.Compile(defaultTemplate)
-	}
-}
-
-func BenchmarkColumnWithAsKeywordCompile(b *testing.B) {
-	c := Column{Name: "role.name AS foo"}
-	for i := 0; i < b.N; i++ {
-		_, _ = c.Compile(defaultTemplate)
 	}
 }
